a235bbd49819af6da3581b65fe6c5a5967d6ddab
sandeepkrpalit <sandeepkrpalit@gmail.com>
2024-12-21 21:07:43 +0530

    Update the changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Projects',
+    version = '0.0.1',
+    author = 'Sandeep Kumar Palit',
+    author_email = 'sandeepkrpalit@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
